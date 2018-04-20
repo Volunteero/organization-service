a03b03d1bf69c58d036d0e23908a98d2f3ee8d65
Jack D <dmarkov23@gmail.com>
2018-04-20 20:20:52 +0200

    repo setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5656;
+
+
+
+// routes go here
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+});
