e52eca86cf88e06809a30bfaba01790970883ba4
Jack D <dmarkov23@gmail.com>
2018-04-23 12:03:52 +0200

    setup of body parser

--- controllers/organization-controller.js
+++ controllers/organization-controller.js
@@ -1,1 +1,12 @@
 const Organization = require('../models/Organization');
+
+
+module.exports = {
+    create: (req, res) => {
+
+        console.log(req.body);
+
+        res.json(req.body);
+        // Organization.create()
+    },
+};
