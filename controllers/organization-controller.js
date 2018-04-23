28eb2aaf6b9759d2154d178529e598edd16f8268
Jack D <dmarkov23@gmail.com>
2018-04-23 23:09:10 +0200

    get organization by id functionality

--- controllers/organization-controller.js
+++ controllers/organization-controller.js
@@ -11,9 +11,28 @@ module.exports = {
             res.sendStatus(201);
 
         }, (err) => {
-            console.log(err);
-
             res.status(400).json(err.message);
         })
     },
+    getAll: (req, res) => {
+        Organization.find({}).then((organizations) => {
+            res.status(200).json(organizations);
+
+        }, err => {
+            res.status(500).json(err.message);
+
+        })
+    },
+    getById: (req, res) => {
+        // Retrieve the id
+        let id = req.params.organizationId;
+
+        Organization.findById(id).then((organization) => {
+            res.status(200).json(organization);
+
+        }).catch((err) => {
+            res.sendStatus(404);
+        })
+    }
+
 };
