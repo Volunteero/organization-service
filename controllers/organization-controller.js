adc99fc1aa93850248c17883113c98e89d0e17aa
Jack D <dmarkov23@gmail.com>
2018-04-24 11:37:19 +0200

    updating organizations functionality

--- controllers/organization-controller.js
+++ controllers/organization-controller.js
@@ -33,6 +33,36 @@ module.exports = {
         }).catch((err) => {
             res.sendStatus(404);
         })
+    },
+    update: (req, res) => {
+        // Retrieve the id
+        let id = req.params.organizationId;
+
+        // Get the new data from the body
+        let newValues = req.body;
+
+        Organization.findById(id).then((organization) => {
+            // let oldOrganization = Object.assign({}, organization);
+
+            organization.set(newValues);
+            organization.save().then(() => {
+
+
+                // if (JSON.stringify(oldOrganization) === JSON.stringify(organization)) {
+                //     res.status(400).json("Nothing was updated. Check if the parameters are correct!");
+                // }
+                // else {
+                res.status(200).json(organization);
+                // }
+
+            }).catch((err) => {
+                res.status(400).json(err.message);
+            })
+
+
+        }).catch((err) => {
+            res.sendStatus(404);
+        })
     }
 
 };
