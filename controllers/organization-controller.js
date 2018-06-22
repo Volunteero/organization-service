17881b62075a688b7c10dc104cfe0e6b94f0310b
Jack D <dmarkov23@gmail.com>
2018-06-22 16:22:48 +0200

    created an endpoint for adding campaigns to organizations

--- controllers/organization-controller.js
+++ controllers/organization-controller.js
@@ -52,54 +52,34 @@ module.exports = {
         })
     },
     update: (req, res) => {
-        // Retrieve the id
-        let id = req.params.organizationId;
-
-        // Get the new data from the body
-        let newValues = req.body;
-
-        if (newValues.hasOwnProperty('event_id')) {
-            Organization.findByIdAndUpdate(id, {$push: {event_ids: newValues.event_id}},
-
-                function (err, model) {
-                    if (err) {
-                        res.send(500).json(err);
-                    }
-                });
-        }
-        else if (newValues.hasOwnProperty('campaign_id')) {
-            Organization.findByIdAndUpdate(id, {$push: {"campaign_ids": newValues.campaign_id}});
-        }
-
-        res.sendStatus(200);
 
 
-        // Organization.findById(id).then((organization) => {
-        //     // let oldOrganization = Object.assign({}, organization);
-        //     // :TODO make a check if nothing was updated
-        //
-        //
-        //     organization.set(newValues);
-        //     organization.save().then(() => {
-        //
-        //
-        //         // if (JSON.stringify(oldOrganization) === JSON.stringify(organization)) {
-        //         //     res.status(400).json("Nothing was updated. Check if the parameters are correct!");
-        //         // }
-        //         // else {
-        //         console.log(organization);
-        //
-        //         res.sendStatus(204);
-        //         // }
-        //
-        //     }).catch((err) => {
-        //         res.status(400).json(err.message);
-        //     })
-        //
-        //
-        // }).catch((err) => {
-        //     res.sendStatus(404);
-        // })
+        Organization.findById(id).then((organization) => {
+            // let oldOrganization = Object.assign({}, organization);
+            // :TODO make a check if nothing was updated
+
+
+            organization.set(newValues);
+            organization.save().then(() => {
+
+
+                // if (JSON.stringify(oldOrganization) === JSON.stringify(organization)) {
+                //     res.status(400).json("Nothing was updated. Check if the parameters are correct!");
+                // }
+                // else {
+                console.log(organization);
+
+                res.sendStatus(204);
+                // }
+
+            }).catch((err) => {
+                res.status(400).json(err.message);
+            })
+
+
+        }).catch((err) => {
+            res.sendStatus(404);
+        })
     }
     ,
     delete:
@@ -133,9 +113,33 @@ module.exports = {
                         res.send(500).json(err);
                     }
                 });
+
+            res.status(200).send('Organization updated');
         }
         else {
             res.status(400).send("You need to have event_id in the body")
+        }
+    },
+    addCampaignToOrganization: (req, res) => {
+        // Retrieve the id
+        let id = req.params.organizationId;
+
+        // Get the new data from the body
+        let newValue = req.body;
+
+        if (newValue.hasOwnProperty('campaign_id')) {
+            Organization.findByIdAndUpdate(id, {$push: {campaign_ids: newValue.campaign_id}},
+
+                function (err, model) {
+                    if (err) {
+                        res.send(500).json(err);
+                    }
+                });
+
+            res.status(200).send('Organization updated');
+        }
+        else {
+            res.status(400).send("You need to have campaign_id in the body")
         }
     }
 
